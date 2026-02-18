bc22032d446d5300c1af3c66e84ecdbc159c9acd
Neil D'Souza <hypercar123456@gmail.com>
2026-02-18 16:56:48 +0530

    feat: modularize tyre sim and add time conversion library

---
+++ time.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int timeFormat(double t)
+{
+ int Minutes,Seconds,Millis;
+ Minutes=(int)t / 60;
+ Seconds=(int)t%60;
+ Millis = round((t - (int)t) * 1000);
+ cout<<Minutes<<"m "<<Seconds<<"s "<<Millis<<"ms";
+ return 0;
+}
---
+++ time.h
@@ -0,0 +1,6 @@
+#ifndef TIME_H
+#define TIME_H
+
+int timeFormat(double t);
+
+#endif
